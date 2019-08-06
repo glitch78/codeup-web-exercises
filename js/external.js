8a4c9807f3d09916589df430ed953710fee6118a
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-08-06 08:37:44 -0500

    updated external js

--- js/external.js
+++ js/external.js
@@ -17,21 +17,21 @@ confirm("Cool!" + " " + favorite + " " + "is my favorite color too.");
 
 
 var littleMermaidDaysRented = prompt("How many days did you rent Little Mermaid?");
-alert("I rented it for" + " " + littleMermaidDaysRented);
+alert("I rented it for" + " " + Number(littleMermaidDaysRented));
 
 var brotherBearDaysRented =prompt("How many days did you rent Brother Bear?");
-alert("I rented it for" + " " + brotherBearDaysRented);
+alert("I rented it for" + " " + Number(brotherBearDaysRented));
 
 var herculesDaysRent = prompt("How many days did you rent Hercules");
-alert("I rented it for" + " " + herculesDaysRent);
+alert("I rented it for" + " " + Number(herculesDaysRent));
 
 var totalDaysRented = (littleMermaidDaysRented + brotherBearDaysRented + herculesDaysRent);
-alert ("The total days rented is:" + " " + totalDaysRented);
+alert ("The total days rented is:" + " " + Number(totalDaysRented));
 
 var pricePerDay = prompt("What is the price per day to rent a move?");
 alert("The price per day is" + " " + pricePerDay);
 
-var rentalTotal = alert("The total cost is" + pricePerDay * totalDaysRented);
+var rentalTotal = alert("The total cost is" + Number(pricePerDay) * Number(totalDaysRented));
 
 
 
@@ -41,7 +41,7 @@ var rentalTotal = alert("The total cost is" + pricePerDay * totalDaysRented);
  */
 
 var totalGooglePayment = prompt ("how much did google pay you?" );
-alert("Google paid me:" + "" + totalGooglePayment);
+alert("Google paid me:" + " " + totalGooglePayment);
 
 
 var totalAmazonPayment = prompt("Amazon paid me: ");
@@ -50,7 +50,7 @@ alert("Amazon paid me:" + " " + totalAmazonPayment);
 var toatalFacebookPayment = prompt("how much did facebook pay you");
 alert("Facebook paid me: " + " " + toatalFacebookPayment);
 
-var totalyCompensation = alert("My total compensation is:" + " "  + toatalFacebookPayment + totalAmazonPayment + totalGooglePayment);
+var totalyCompensation = alert("My total compensation is:" + " "  + Number(toatalFacebookPayment) + Number(totalAmazonPayment) + Number(totalGooglePayment));
 
 
 var classIsnotFull = true;
