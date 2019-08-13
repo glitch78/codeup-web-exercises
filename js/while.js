b2ec2bb0bc6e10819acb1e5988a0d09d43db906b
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-08-13 10:37:48 -0500

    while ls

--- js/while.js
+++ js/while.js
@@ -5,3 +5,20 @@ while (i < 65536){
     console.log(i);
 }
 
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+
+
+console.log("Let's get to work, I've got" + allCones + "conse to sell")
+do {
+    var conseSold = Math.floor(Math.random() * 5) + 1;
+    if (conseSold > allCones) {
+        console.log("Sorry, I can't sell to you" + conseSold + "I've only got " + allCones + "left")
+    }else {
+        allCones = allCones - conseSold;
+        console.log("Here you are!" + conseSold + "cones only" + allCones + "left to sell")
+    }
+} while (allCones > 0) ;
+
+console.log("All cones sold, let's go home");
+
