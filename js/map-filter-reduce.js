f87d95151de0f4be968fc98f224dc3c1b379ea45
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-09 08:44:23 -0500

    add map filter reduce exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,71 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+users.filter(user =>{
+    if(user.languages.length>=3){
+        return user;
+    }
+});
+
+users.map(user=>{
+    return user.email;
+});
+
+let average = users.reduce((total,user)=>{
+    return total+user.yearsOfExperience;
+},0)/users.length;
+
+users.reduce((longest,user)=>{
+    if(user.email.length > longest.length){
+        longest=user.email;
+    }
+    return longest;
+},"a");
+
+users.reduce((names,user)=>{
+    return names+user.name+", "
+},"Your instructors are: ");
+
+users.reduce((languages,user)=>{
+    user.languages.forEach(language=>{
+        if (!languages.includes(language)){
+            languages.push(language);
+        }
+    });
+    return languages
+},[]);
