e341088a77920e814a672fba02c7c5160bb1a451
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-09-10 10:41:45 -0500

    add promises exercise

---
+++ js/promises.js
@@ -0,0 +1,26 @@
+//wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+// const myPromise = new Promise((resolve, reject) => {
+//     if (Math.random() > 0.5) {
+//         resolve();
+//     } else {
+//         reject();
+//     }
+// });
+
+
+
+const wait = (ms) => {
+    const myPromise = new Promise((resolve, reject) => {
+        setTimeout(function () {
+            resolve()
+        },ms)
+
+    });
+
+    return myPromise;
+};
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
